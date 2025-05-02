a100d649b4093108a0c2da811e70b46e4a5b569d
bielbritob <bielbritob123@gmail.com>
2025-05-02 13:34:54 -0400

    commit #26 - tesseract fixes

--- app2/appGSheet.py
+++ app2/appGSheet.py
@@ -533,7 +533,7 @@ def main():
 
                 #================ DEBUG LOCAL ONLY ===================
                 #pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
-
+                pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
                 # --- funcoes de parsing ---
                 # padrões da sua descrição de payload
                 KEYWORDS_DESC = ["DiaDJogos", "Terceirinhos", "PYPIX", "Pix"]
